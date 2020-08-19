ae11e18e22c960f4db50c6d66e68b74b4ae7c304
Jesus462 <jesusponce.462@gmail.com>
2020-08-18 20:06:38 -0400

    sass added

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import '../styles/index.css';
+import "../styles/index.scss";
 
 //import your own components
 import Layout from "./Layout";
